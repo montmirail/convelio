f5f99b62090f05d6de891b383200edc2346330a8
Paul-Emile Joëssel <pejoessel@gmail.com>
2020-07-10 15:56:45 +0200

    Add log

--- src/convelio.ts
+++ src/convelio.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 import { PriceRequestDto } from './dto/price-request.dto';
 import { CalculateResponse } from './models/calculate-response';
 
@@ -12,20 +12,23 @@ export class Convelio {
   }
 
   calculate(input: PriceRequestDto): Promise<CalculateResponse> {
-    return this.sendRequest(this.calculatePriceUrl, input).then(({ data }) => {
-      if (data.err !== 0) {
-        throw new Error(data.err.msg);
-      }
+    return this.sendRequest(this.calculatePriceUrl, input)
+      .then(({ data }) => {
+        console.log(data);
 
-      return data.data;
-    });
+        if (data.err !== 0) {
+          throw new Error(data.err);
+        }
+
+        return data.data;
+      });
   }
 
   placeShippingOrder(): Promise<any> {
     return this.sendRequest(this.placeOrderUrl, {});
   }
 
-  private sendRequest(url: string, data: any): Promise<any> {
+  private sendRequest(url: string, data: any): Promise<AxiosResponse<any>> {
     return axios.post(url, data, {
       headers: {
         'Accept-Encoding': 'gzip, deflate',
